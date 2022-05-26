4cb4ffd6fb4cb4cf217126b7471a195aa2395ed0
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-05-26 15:36:50 -0700

    custom-roll

--- custom-roll.js
+++ custom-roll.js
@@ -3,18 +3,7 @@ const prompt = require('prompt-sync')({
 });
 
 
-let userInput = (prompt("How many sides should the dice have? (choose between 2,4,6,8,10): "));
+let userInput = (prompt("How many sides should the dice have?: "));
 
-if (userInput === "6"){
-    console.log(Math.ceil(Math.random()*6));
-} else if(userInput === "2"){
-    console.log(Math.ceil(Math.random()*2));
-} else if(userInput === "4"){
-    console.log(Math.ceil(Math.random()*4));
-} else if(userInput === "8"){
-    console.log(Math.ceil(Math.random()*8));
-} else if (userInput === "10"){
-    console.log(Math.ceil(Math.random()*10));
-} else {
-    console.log("error");
-}
+let userInput2 = Math.ceil(Math.random()*userInput);
+console.log(userInput2)
